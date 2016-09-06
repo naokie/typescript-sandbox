1adf4e066c293f2791961207cbf49f7b4793af12
Naoki Endoh <naoki.endoh@gmail.com>
2016-09-07 01:37:55 +0900

    Add type annotations

--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,6 @@
-function greeter(person) {
+function greeter(person: string) {
   return "Hello, " + person;
 }
 
-var user = "Jane User";
+var user = [0, 1, 2];
 document.body.innerHTML = greeter(user);
