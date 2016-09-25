f1243100291035df139a1238991c332b20cfbaae
Naoki Endoh <naoki.endoh@gmail.com>
2016-09-26 01:50:24 +0900

    Add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 var tsify = require('tsify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
 var paths = {
   pages: ['src/*.html']
@@ -28,6 +31,22 @@ function bundle() {
     .pipe(gulp.dest('dist'));
 }
 
-gulp.task('default', ['copy-html'], bundle);
+gulp.task('default', ['copy-html'], function() {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+  })
+  .plugin(tsify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .piipe(uglify())
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('dist'));
+});
 watchedBrowserify.on('update', bundle);
 watchedBrowserify.on('log', gutil.log);
