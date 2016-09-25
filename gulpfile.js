8771366ea62a658e7a1f75c58980dcf576085490
Naoki Endoh <naoki.endoh@gmail.com>
2016-09-26 02:13:07 +0900

    Add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var watchify = require('watchify');
 var tsify = require('tsify');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
 var paths = {
   pages: ['src/*.html']
 };
-
-var watchedBrowserify = watchify(browserify({
-  basedir: '.',
-  debug: true,
-  entries: ['src/main.ts'],
-  cache: {},
-  packageCache: {}
-}).plugin(tsify));
 
 gulp.task('copy-html', function() {
   return gulp.src(paths.pages)
     .pipe(gulp.dest('dist'));
 });
-
-function bundle() {
-  return watchedBrowserify
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'));
-}
 
 gulp.task('default', ['copy-html'], function() {
   return browserify({
@@ -40,13 +23,14 @@ gulp.task('default', ['copy-html'], function() {
     packageCache: {}
   })
   .plugin(tsify)
+  .transform('babelify', {
+    presets: ['es2015'],
+    extensions: ['.ts']
+  })
   .bundle()
   .pipe(source('bundle.js'))
   .pipe(buffer())
   .pipe(sourcemaps.init({loadMaps: true}))
-  .piipe(uglify())
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('dist'));
 });
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', gutil.log);
